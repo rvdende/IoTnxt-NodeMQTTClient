8dcac39b56aa96206e22b02d84d7c441d5662b76
Tobievi <30435811+Tobievi@users.noreply.github.com>
2018-10-02 17:24:29 +0200

    Updated the correct main for tutorials

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,14 @@
 import * as iotnxt from "./iotnxt/iotnxt";
 
 var deviceTree = {};
-deviceTree["asdf|1:zxcv|1"] = {
-  Make: null,
-  Model: null,
-  DeviceName: "asdf|1:zxcv|1",
-  DeviceType: "asdf",
+deviceTree["House|1:Appliances|1"] = {
+  Make: "Pi Gateway",
+  Model: "Pi 3 B",
+  DeviceName: "Kichen|1:Stove|1",
+  DeviceType: "Stove",
   Properties: {
-    hello: {
-      PropertyName: "hello",
+    StoveLight: {
+      PropertyName: "StoveLight",
       DataType: null
     }
   }
@@ -16,12 +16,14 @@ deviceTree["asdf|1:zxcv|1"] = {
 
 var iotnxtqueue = new iotnxt.IotnxtQueue(
   {
-    GatewayId: "ROUANTEST",
-    secretkey: "asdf1234zxcv",
+    //Gateway Id , this MUST be unique for every pi/gateway unit
+    GatewayId: "TutorialGateway01",
+    //secret is your password to connect the Gateway, this cannot be changed , dont share with anyone!
+    secretkey: "166123181241239",
     FirmwareVersion: "5.0.25",
-    Make: "IoT.nxt",
+    Make:  "Pi Gateway",
     Model: "nodejs client",
-    id: "rouan test",
+    id: "Pi 3 B",
     publickey: "<RSAKeyValue><Exponent>AQAB</Exponent><Modulus>rbltknM3wO5/TAEigft0RDlI6R9yPttweDXtmXjmpxwcuVtqJgNbIQ3VduGVlG6sOg20iEbBWMCdwJ3HZTrtn7qpXRdJBqDUNye4Xbwp1Dp+zMFpgEsCklM7c6/iIq14nymhNo9Cn3eBBM3yZzUKJuPn9CTZSOtCenSbae9X9bnHSW2qB1qRSQ2M03VppBYAyMjZvP1wSDVNuvCtjU2Lg/8o/t231E/U+s1Jk0IvdD6rLdoi91c3Bmp00rVMPxOjvKmOjgPfE5LESRPMlUli4kJFWxBwbXuhFY+TK2I+BUpiYYKX+4YL3OFrn/EpO4bNcI0NHelbWGqZg57x7rNe9Q==</Modulus></RSAKeyValue>",
     hostaddress: "greenqueue.prod.iotnxt.io"
   },
@@ -33,7 +35,7 @@ var iotnxtqueue = new iotnxt.IotnxtQueue(
 
 iotnxtqueue.on("connect", ()=>{
   console.log("CONNECTED...")
-
+//The following interval will send values to the pllatfor every 2,5 seconds 
   setInterval(()=>{
     console.log("sending data...")
     iotnxtqueue.updateState("asdf|1:zxcv|1", "hello", Math.random())
